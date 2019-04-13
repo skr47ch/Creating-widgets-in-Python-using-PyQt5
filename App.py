2e63036ae8ab6d726b42ec15620e10098baf3e99
skr47ch <its.me.arrived@gmail.com>
2019-04-13 23:46:52 +1000

    Added frames for ribbon, panel and display window

--- App.py
+++ App.py
@@ -1,10 +1,10 @@
 import sys
-from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget
-from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtWidgets import QMainWindow, QApplication, QFrame, QDesktopWidget
 import Menubar
-# import LeftPanel as panel
 
+MENU_HEIGHT = 25
+RIBBON_HEIGHT = 40
+PANEL_WIDTH = 175
 
 class App(QMainWindow):
     def __init__(self):
@@ -19,17 +19,34 @@ class App(QMainWindow):
     def init_ui(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
+        window_size = QDesktopWidget().screenGeometry(-1)
 
-        self.menu_bar = self.menuBar()
-        Menubar.create_menu(self.menu_bar)
+        menu_bar = self.menuBar()
+        Menubar.create_menu(menu_bar)
 
+        # Top Ribbon
+        ribbon = QFrame(self)
+        ribbon.move(0, MENU_HEIGHT)
+        ribbon.resize(window_size.width(), RIBBON_HEIGHT)
+        ribbon.setStyleSheet('background-color: gray')
+
+        # Left Pane
+        left = QFrame(self)
+        left.move(0, RIBBON_HEIGHT+MENU_HEIGHT)
+        left.resize(PANEL_WIDTH, window_size.height())
+        left.setStyleSheet('background-color: yellow')
+
+        # Display Window
+        right = QFrame(self)
+        right.move(PANEL_WIDTH, RIBBON_HEIGHT+MENU_HEIGHT)
+        right.resize(window_size.width(), window_size.height())
+        right.setStyleSheet('background-color: pink')
 
 def main():
     app = QApplication([sys.argv])
     root = App()
     root.show()
     sys.exit(app.exec_())
-
 
 if __name__ == '__main__':
     main()
